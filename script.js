b6a7f16c0c638d87cc9e483ba8c607e4ab7c23c5
Laksh <91580291+lakshRP@users.noreply.github.com>
2023-11-19 22:08:05 -0600

    Update script.js
    new js

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-document.addEventListener("DOMContentLoaded", function() {
+
   const slides = document.querySelectorAll(".slide");
   let currentSlide = 0;
-
+   const bar = document.getElementById("topbar");
   function showSlide(index) {
-    if (index >= 0 && index < slides.length) {
-      slides.forEach((slide) => {
-        slide.style.transform = `translateY(-${index * 100}vh)`;
-      });
-      currentSlide = index;
-    }
+    console.log(index)
+      if (index == 1){
+
+        bar.style.zIndex = 200;
+      }
+      
+      if (index >= 0 && index < slides.length) {
+        slides.forEach((slide) => {
+          slide.style.transform = `translateY(-${index * 100}vh)`;
+        });
+        currentSlide = index;
+
+      }
+    
+    
+  }
+  
+  function DownButton(){
+     showSlide(currentSlide + 1);
+  }
+  function UpButton(){
+    showSlide(currentSlide - 1);
   }
 
   document.addEventListener("wheel", function(event) {
@@ -22,8 +38,9 @@ document.addEventListener("DOMContentLoaded", function() {
   document.addEventListener("keydown", function(event) {
     if (event.key === "ArrowDown") {
       showSlide(currentSlide + 1);
+       
     } else if (event.key === "ArrowUp") {
       showSlide(currentSlide - 1);
+
     }
   });
-});
